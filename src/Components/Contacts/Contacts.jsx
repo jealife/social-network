b23db5a45a8feff708eca5efcc513ed49d0b3ce6
jealife <jeanguylanememiaghe@gmail.com>
2023-09-14 19:25:17 +0100

    modified:   src/Components/Contacts/Contacts.jsx
     Changes not staged for commit:
    	modified:   src/Components/Contacts/Contacts.jsx

--- src/Components/Contacts/Contacts.jsx
+++ src/Components/Contacts/Contacts.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import Contact from './Contact'
 
 export default function Contacts() {
-  return (
-    <div className="online">
-          <h6 className="title">contact</h6>
-          <Contact avatar='img/avatar/1.jpg' name='diana berry'/>
+    return (
+        <div className="online">
+            <h6 className="title">contact</h6>
+            <Contact avatar='img/avatar/1.jpg' name='diana berry' />
+            <Contact avatar='img/avatar/2.jpg' name='samantha jones' />
+            <Contact avatar='img/avatar/3.jpg' name='john doe' />
+            <Contact avatar='img/avatar/4.jpg' name='stacy jr.' />
+            <Contact avatar='img/avatar/5.jpg' name='christin sam' />
+            <Contact avatar='img/avatar/6.jpg' name='kate simon' />
+            <Contact avatar='img/avatar/7.jpg' name='diana berry' />
         </div>
-  )
+    )
 }
